522f4a9a8ee789fc6f13ec773e9475aca456bc98
harsh996 <harsh020596@gmail.com>
2017-04-03 15:38:48 +0530

    Update integrated_node_raspi.py

--- integrated_node_raspi.py
+++ integrated_node_raspi.py
@@ -13,15 +13,15 @@ def on_message(client, userdata, msg):
        print("Message arrived from "+str(client))
        print(msg.topic+" "+str(msg.payload))
 client = mqtt.Client()
-x=client.connect("192.168.0.107",1883,60)
+x=client.connect("IP address of the broker",1883,60)
 client.on_connect = on_connect
 client.on_message = on_message
 
 #Publish to the topic "temp/led"
 print("Sending 0........")
-publish.single("temp/led","0",hostname="192.168.0.107")
+publish.single("temp/led","0",hostname="IP address of the broker")
 time.sleep(30)
 print("Sending 1........")
-publish.single("temp/led", "1", hostname="192.168.0.107")
+publish.single("temp/led", "1", hostname="IP address of the broker")
 
 client.loop_forever()
