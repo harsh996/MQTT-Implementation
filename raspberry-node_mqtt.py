6e8bab46a566594f96000934f9f458dcc157f046
harsh996 <harsh020596@gmail.com>
2017-04-03 15:59:05 +0530

    Update raspberry-node_mqtt.py

--- raspberry-node_mqtt.py
+++ raspberry-node_mqtt.py
@@ -1,7 +1,7 @@
 import paho.mqtt.publish as publish
 import time
 print("Sending 0........")
-publish.single("ledstatus","0",hostname="192.168.0.107")
+publish.single("ledstatus","0",hostname="IP of Broker")
 time.sleep(20)
 print("Sending 1........")
-publish.single("ledstatus", "1", hostname="192.168.0.107")
+publish.single("ledstatus", "1", hostname="IP of Broker")
