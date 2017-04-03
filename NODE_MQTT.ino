61eede6ff93836c2ea8f71d8c1b8f9e1089068a3
harsh996 <harsh020596@gmail.com>
2017-04-03 16:05:35 +0530

    Add files via upload

---
+++ NODE_MQTT.ino
@@ -0,0 +1,93 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into pin D4 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices 
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+// Update these with values suitable for your network.
+const char* ssid = "SMART_JOULES_24";
+const char* password = "joulesO23";
+const char* mqtt_server = "192.168.0.107";
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+float temp = 0;
+int inPin = 5;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("arduinoClient_temperature_sensor")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+ 
+void setup()
+{
+  Serial.begin(115200);
+  setup_wifi(); 
+  client.setServer(mqtt_server, 1883);
+  pinMode(inPin, INPUT);
+  sensors.begin();
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 30000) {
+    lastMsg = now;
+    sensors.setResolution(12);
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    temp = sensors.getTempCByIndex(0);
+    Serial.println(temp);
+    if((temp > -20) && (temp <60))
+      {
+     
+      client.publish("t1", String(temp).c_str(),TRUE);
+      }
+  }
+}
+
